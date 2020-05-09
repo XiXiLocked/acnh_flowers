715b390e0178c0d52e6d9d858ec81b6c9a28e6bc
XiXiLocked <xijunyi1990@gmail.com>
2020-05-09 23:54:35 +0800

    refactor

--- make_table.py
+++ make_table.py
@@ -4,47 +4,41 @@ from collections import Counter
 
 def cross_pattern_one():
     t={'00':'00', '01':'01','10':'01', '11':'11'}
-    cross = { i+j: list(map(lambda x:t[x[0]+x[1]],product(*zip(*i),*zip(*j))))for i,j in product(['00','01','11'], ['00','01','11'])}
+    gene = ['00','01','11']
+    cross = {}
+    for i,j in product(gene,gene):
+        cross[i+j] =  [  t[x[0]+x[1]] for x in product(list(*zip(*i)), list(*zip(*j)))]
     return cross
 
 cross_pattern = cross_pattern_one()
 
-def crossing3(gene_a, gene_b):
+def split_gene(gene):
+    return [gene[i:i+2] for i in range(0,len(gene),2)]
+
+def crossing(gene_a, gene_b):
     cc = Counter()
-    x0,y0,z0 = gene_a[:2], gene_a[2:4], gene_a[4:6]
-    x1,y1,z1 = gene_b[:2], gene_b[2:4], gene_b[4:6]
-    for v in product(cross_pattern[x0+x1],
-                     cross_pattern[y0+y1],
-                     cross_pattern[z0+z1]):
+    split_a = split_gene(gene_a)
+    split_b = split_gene(gene_b)
+    segments = [cross_pattern[a+b] for a,b in zip(split_a,split_b)]
+    for v in product(*segments): 
         gene_type = ''.join(v)
         cc[gene_type]+=1
     return cc
+ 
 
-def crossing4(gene_a, gene_b):
-    cc = Counter()
-    x0,y0,z0,w0 = gene_a[:2], gene_a[2:4], gene_a[4:6], gene_a[6:8]
-    x1,y1,z1,w1 = gene_b[:2], gene_b[2:4], gene_b[4:6], gene_b[6:8]
-    for v in product(cross_pattern[x0+x1],
-                     cross_pattern[y0+y1],
-                     cross_pattern[z0+z1],
-                     cross_pattern[w0+w1]):
-        gene_type = ''.join(v)
-        cc[gene_type]+=1
-    return cc      
-
-def crossing_table(genetypes, crossing_rules, filename):
+def crossing_table(genetypes, filename):
     lines = []
     for parent_a in genetypes:
         for parent_b in genetypes:
             line = []
-            children = crossing_rules(parent_a, parent_b)
+            children = crossing(parent_a, parent_b)
             line.append(parent_a)
             line.append(parent_b)
             for child in children:
                 c = children[child]
                 line.append((child, c))
             lines.append(line)
-    
+
     with open(filename,'w') as f:
         for l in lines:
             f.write("%s,%s,"%(l[0],l[1]))
@@ -58,7 +52,7 @@ def crossing3_table():
     for v in product(*[gene]*3):
         gene_type = ''.join(v)
         genetypes.append(gene_type)
-    crossing_table(genetypes,crossing3, 'table3.csv')
+    crossing_table(genetypes, 'csv/table3.csv')
 
 def crossing4_table():
     genetypes = []
@@ -66,7 +60,7 @@ def crossing4_table():
     for v in product(*[gene]*4):
         gene_type = ''.join(v)
         genetypes.append(gene_type)
-    crossing_table(genetypes,crossing4, 'table4.csv')
+    crossing_table(genetypes, 'csv/table4.csv')
 
 
 if __name__ == "__main__":
